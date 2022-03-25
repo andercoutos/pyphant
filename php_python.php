89534525d47ee093d378b10910e1d019afee828f
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2022-03-25 20:52:59 -0300

    Create php_python.php

---
+++ php_python.php
@@ -0,0 +1,13 @@
+<?php
+print 'hello world em php 1';
+?>
+
+<?phpython
+print('hello world em python 1')
+phpython?><?php
+print 'hello world em php 2';
+?>
+
+<?phpython
+print('hello world em python 2')
+phpython?>
